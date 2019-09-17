729a4a888483317cca1d65e114984f3be6f0fe66
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 13:53:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ class Store{
     return Store.singleton;
   }
 }
+
+class Driver {
+  static get counter(){
+    Driver._counter = (Driver._counter || 0) + 1;
+    return Driver._counter;
+  }
+
+  constructor(name){
+    this.name = name;
+    this._id = Driver.counter;
+    this.tripsArr = []; 
+    Store.store.drivers.push(this);
+  }
+
+  get id(){
+    return this._id;
+  }
+
+  addTrip(trip){
+    this.tripsArr.push(trip);
+  }
+
+  trips(){
+    return Store.store.trips.filter((trip) => trip.driver === this)
+  }
+}
