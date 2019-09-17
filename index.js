bfbf597d30d2fb1b6f375a9d687cdc27e9e375d9
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 14:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ class Driver {
     return Store.store.passengers.filter(p => p.trips().filter(trip => trip.driver() === this).map(trip => trip.passenger()));
   }
 }
+
+class Passenger{
+  static get counter(){
+    Passenger._counter = (Passenger._counter || 0) + 1;
+    return Passenger._counter;
+  }
+
+  constructor(name){
+    this.name = name;
+    this._id = Passenger.counter;
+    Store.store.passengers.push(this);
+  }
+
+  get id(){
+    return this._id;
+  }
+}
