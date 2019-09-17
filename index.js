6c92ee42d55b54f572cded62fb2470d36bf7e0be
Donna Amos <donnacamos.developer@gmail.com>
2019-09-17 14:02:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,62 +1,117 @@
 class Store{
-  constructor(){
-    this.drivers = [];
-    this.passengers = [];
-    this.trips = [];
-    this.driverId = 1;
-    this.passengerId = 1;
-    this.tripId = 1;
-  }
 
-  static get store(){
-    Store.singleton = Store.singleton || new Store();
-    return Store.singleton;
-  }
+    constructor(){
+        this.drivers = [];
+        this.passengers = [];
+        this.trips = [];
+        this.driverId = 1;
+        this.passengerId = 1;
+        this.tripId = 1;
+    }
+
+    static get store(){
+        Store.singleton = Store.singleton || new Store(); 
+        return Store.singleton;
+    }
+
 }
 
-class Driver {
-  static get counter(){
-    Driver._counter = (Driver._counter || 0) + 1;
-    return Driver._counter;
-  }
 
-  constructor(name){
-    this.name = name;
-    this._id = Driver.counter;
-    this.tripsArr = [];
-    Store.store.drivers.push(this);
-  }
+class Driver{
 
-  get id(){
-    return this._id;
-  }
+    static get counter(){
+        Driver._counter = (Driver._counter || 0) + 1;
+        return Driver._counter;
+    }
 
-  addTrip(trip){
-    this.tripsArr.push(trip);
-  }
+    constructor(name){
+        this.name = name;
+        this._id = Driver.counter;
+        this.tripsArr = [];
+        Store.store.drivers.push(this);
+    }
 
-  trips(){
-    return Store.store.trips.filter((trip) => trip.driver() === this);
-  }
+    get id(){
+        return this._id;
+    }
 
-  passengers(){
-    return Store.store.passengers.filter(p => p.trips().filter(trip => trip.driver() === this).map(trip => trip.passenger()));
-  }
+    addTrip(trip){
+        this.tripsArr.push(trip);
+    }
+
+    trips(){
+        return Store.store.trips.filter((trip) => trip.driver() === this);
+    }
+
+    passengers(){
+        return Store.store.passengers.filter(p => p.trips().filter(trip => trip.driver() === this).map(trip => trip.passenger()));
+    }
 }
+
 
 class Passenger{
-  static get counter(){
-    Passenger._counter = (Passenger._counter || 0) + 1;
-    return Passenger._counter;
-  }
 
-  constructor(name){
-    this.name = name;
-    this._id = Passenger.counter;
-    Store.store.passengers.push(this);
-  }
+    static get counter(){
+        Passenger._counter = (Passenger._counter || 0) + 1;
+        return Passenger._counter;
+    }
 
-  get id(){
-    return this._id;
-  }
+    constructor(name){
+        this.name = name;
+        this._id = Passenger.counter;
+        Store.store.passengers.push(this);
+
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    trips(){
+        return Store.store.trips.filter(trip => trip.passenger() === this);
+    }
+
+    drivers(){
+        return Store.store.trips.filter(trip => trip.passenger() === this).map(trip => trip.driver());
+    }
 }
+
+class Trip{
+
+    static get counter(){
+        Trip._counter = (Trip._counter || 0) + 1;
+        return Trip._counter;
+    }
+
+    constructor(driver, passenger){
+        this._id = Trip.counter;
+        this.driverObj = driver;
+        this.passengerObj = passenger;
+        this.driverId = driver.id;
+        this.passengerId = passenger.id;
+        Store.store.trips.push(this);
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    driver(){
+        return this.driverObj;
+    }
+
+    passenger(){
+        return this.passengerObj;
+    }
+
+}
+
+store = Store.store;
+
+driver = new Driver("micah");
+passenger = new Passenger("kristina");
+trip = new Trip(driver, passenger);
+console.log(passenger);
+console.log(driver);
+console.log(Store.store);
+console.log(driver.trips());
